54daffbc49b3600144abd211a02d1e041976d01b
sheepjin11 <sheepjin11@gmail.com>
2026-06-17 20:13:14 +0900

    TreePass artifact: v1.1

---
+++ bench/CMakeFiles/micro_check_tail.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/micro_check_tail.dir/micro_check_tail.cpp.o"
+  "micro_check_tail"
+  "micro_check_tail.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/micro_check_tail.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
